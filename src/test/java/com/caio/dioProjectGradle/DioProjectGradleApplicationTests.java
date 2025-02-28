751d8f24730578190d68d782f7c6715f8be9ca30
Caio Prado de Souza <cpssouza7@gmail.com>
2025-02-28 10:42:57 -0300

    Feat: criado projeto DIO com GRADLE

---
+++ src/test/java/com/caio/dioProjectGradle/DioProjectGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio.dioProjectGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioProjectGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
